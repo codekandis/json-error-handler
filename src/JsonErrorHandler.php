98556ed09d24f8e2409e5a5048e0272238f01464
Christian Ramelow <info@codekandis.net>
2019-08-11 18:24:19 +0200

    Adds the `JsonErrorHandler`.

---
+++ src/JsonErrorHandler.php
@@ -0,0 +1,27 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\JsonErrorHandler;
+
+use JsonException;
+use function json_last_error;
+
+/**
+ * Represents a JSON error handler.
+ * @package codekandis/json-error-handler
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class JsonErrorHandler implements JsonErrorHandlerInterface
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	public function handle(): void
+	{
+		$errorCode = json_last_error();
+		if ( JsonErrorCodes::NONE !== $errorCode )
+		{
+			$errorMessage = ( new JsonErrorCodesInterpreter() )
+				->interpret( $errorCode );
+			throw new JsonException( $errorMessage, $errorCode );
+		}
+	}
+}
