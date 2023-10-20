e77ff5f11963f06a741720910fa272b1a32e1f5b
Christian Ramelow <info@codekandis.net>
2023-10-20 20:38:48 +0200

    Refactors the unit tests.

---
+++ tests/Fixtures/InvalidValues.php
@@ -0,0 +1,16 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\JsonErrorHandler\Tests\Fixtures;
+
+/**
+ * Represents an enumeration of invalid values.
+ * @package codekandis/json-error-handler
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+abstract class InvalidValues
+{
+	/**
+	 * Represents an invalid JSON string.
+	 * @var string
+	 */
+	public const string JSON_STRING = '{"foobar":}';
+}
